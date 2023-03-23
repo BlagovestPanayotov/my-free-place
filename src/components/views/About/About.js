826262943cedc655e224c70785f44886a4e0f368
BlagovestPanayotov <83649155+BlagovestPanayotov@users.noreply.github.com>
2023-03-23 11:15:01 +0000

    Fix buttons on About and profile view

--- src/components/views/About/About.js
+++ src/components/views/About/About.js
@@ -2,12 +2,12 @@ import { onBackClick } from '../../../utils/util';
 import styles from './About.module.css';
 
 
-function About({navigate}) {
+function About({ navigate }) {
     return (
         <div className={styles.content}>
             <h1><i>Thank you for visiting my application</i></h1>
             <p>This application is created as proof of skils working with React js by Blagovest Panayotov.</p>
-            <button onClick={e => onBackClick(e, navigate)}>Back</button>
+            <button id={styles['back-btn']} onClick={e => onBackClick(e, navigate)}>Back</button>
         </div>
     );
 }
